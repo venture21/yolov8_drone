3f6d694f9826056858ce79d05aaf3382a98db1b3
venture21 <park0122@gmail.com>
2023-06-24 14:39:16 +0900

    trainTestSplit.py파일 추가

--- trainTestSplit.py
+++ trainTestSplit.py
@@ -1,64 +1,47 @@
 import glob, os
 import shutil
+import random
 
-# 현재 디렉토리를 읽어온다.
-current_dir = os.path.dirname(os.path.abspath(__file__))
-print("현재 작업 디렉토리 : "+current_dir)
+# �꾩옱 �붾젆�좊━瑜� �쎌뼱�⑤떎.
+#current_dir = os.path.dirname(os.path.abspath(__file__))
 
-current_dir = 'maskDataset'
 
-# train : valid: test ratio
-# 80 : 10 : 10
+datasetDir = 'C:\\dataset\\drone_org'
+print('Dataset Directory is '+datasetDir)
+
+targetDir = 'C:\\dataset\\drone_new'
+
+trainImagePath = os.path.join(targetDir, 'train','images')
+trainLabelPath = os.path.join(targetDir, 'train','labels')
+validImagePath = os.path.join(targetDir, 'valid','images')
+validLabelPath = os.path.join(targetDir, 'valid','labels')
+testImagePath = os.path.join(targetDir, 'test','images')
+testLabelPath = os.path.join(targetDir, 'test','labels')
+
+# train : test ratio
+# 90 : 10
 valid_ratio = 10
 test_ratio = 10
 
-# Create and/or truncate train.txt and test.txt
-file_train = open('train.txt', 'w')
-file_test = open('test.txt', 'w')
-
 # Populate train.txt and test.txt
 counter = 1
-
 index_test = round(100 / test_ratio)
-index_valid = round((100-index_test)/valid_ratio)
-
-
-def makeFileList():
-    # current_dir의 jpg파일만 골라내서 train.txt, trest.txt로 파일을 생성
-    for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
-        # # title은 파일명에서 확장자만 제외한 파일명, ext는 확장자
-        # os.path.basename()함수는 경로+파일명 -> 파일명만 추출해내는 함수
-        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
-
-        # 10번에 한번 test리스트에 추가
-        if counter == index_test:
-            counter = 1
-            # 경로+파일명
-            file_test.write(current_dir + "/" + title + '.jpg' + "\n")
-
-        # 9번은 train리스트에 추가
-        else:
-            # 경로+파일명
-            file_train.write(current_dir + "/" + title + '.jpg' + "\n")
-            counter = counter + 1
+index_valid = round((100-index_test) / valid_ratio)
 
 def makeFolder():
-    # train/valid/test 폴더를 생성하고, 하위에 images, labels폴더를 생성
-    # 기존에 폴더가 존재하면 폴더와 그안의 모든 파일 삭제
-    trainImagePath = current_dir+'/train/images'
-    trainLabelPath = current_dir+'/train/labels'
-    validImagePath = current_dir+'/valid/images'
-    validLabelPath = current_dir+'/valid/labels'
-    testImagePath = current_dir+'/test/images'
-    testLabelPath  = current_dir+'/test/labels'
+    # train/valid/test로 나누기
+    # train/
+    #        images/
+    #        labels/
+    # valid/
+    #        images/
+    #        labels/
+    # test/
+    #        images/
+    #        labels/
+    if os.path.exists(targetDir):
+        shutil.rmtree(targetDir)
 
-    if os.path.exists(trainImagePath):
-        shutil.rmtree(trainImagePath)
-        shutil.rmtree(trainLabelPath)
-        shutil.rmtree(validImagePath)
-        shutil.rmtree(validLabelPath)
-        shutil.rmtree(testImagePath)
-        shutil.rmtree(testLabelPath)
         print("old file is removed")
 
     os.makedirs(trainImagePath, exist_ok=True)
@@ -68,77 +51,52 @@ def makeFolder():
     os.makedirs(testImagePath, exist_ok=True)
     os.makedirs(testLabelPath, exist_ok=True)
 
-def copyDataset():
-    global counter
-
-    # current_dir의 jpg파일만 골라내서 train.txt, trest.txt로 파일을 생성
-    # 경로+파일명+jpg
-    for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
-        # # title은 파일명에서 확장자만 제외한 파일명, ext는 확장자
-        # os.path.basename()함수는 경로+파일명 -> 파일명만 추출해내는 함수
-        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
-
-        #txt파일의 경로 문자열 만들기
-        pathString = os.path.dirname(pathAndFilename)
-        #labelFile = pathString+'/'+title+'.txt'
-        labelFilename = os.path.join(pathString,title+'.txt')
-        
-        # 각 폴더로 파일들을 복사 : 이미지를 먼저 복사하고(images폴더로 복사), 
-        # 확장자만 txt인 파일을 찾아서 labels폴더로 복사 
-        
-        # counter가 10이되면 test 리스트에 추가
-        # counter =1 로 초기화
-        if counter == index_test:
-            counter = 1
-            #경로+파일명을 받아와서 파일을 이동
-            #print(current_dir+'/test/images/'+title+'.jpg')
-            #shutil.move(pathAndFilename, current_dir+'/test/images/'+title+'.jpg')
-            shutil.copyfile(pathAndFilename, current_dir+'/test/images/'+title+'.jpg')
-            shutil.copyfile(labelFilename, current_dir+'/test/labels/'+title+'.txt')
-  
-        # counter 9가되면 valid
-        elif counter == index_valid:
-            shutil.copyfile(pathAndFilename, current_dir+'/valid/images/'+title+'.jpg')
-            shutil.copyfile(labelFilename, current_dir+'/valid/labels/'+title+'.txt')
-            counter = counter + 1
-
-        # counter가 0~8일때는 train 리스트에 추가
-        else:
-            #print(current_dir+'/train/images/'+title+'.jpg')
-            #shutil.move(pathAndFilename, current_dir+'/train/images/'+title+'.jpg')
-            shutil.copyfile(pathAndFilename, current_dir+'/train/images/'+title+'.jpg')
-            shutil.copyfile(labelFilename, current_dir+'/train/labels/'+title+'.txt')
-            counter = counter + 1
-
-#복사가 잘 됐는지 폴더별로 파일의 갯수를 확인
-def checkCopy():
-    trainImageList = os.listdir(current_dir+'/train/images')
-    trainLabelList = os.listdir(current_dir+'/train/labels')
-    validImageList = os.listdir(current_dir+'/valid/images')
-    validLabelList = os.listdir(current_dir+'/valid/labels')
-    testImageList = os.listdir(current_dir+'/test/images')
-    testLabelList = os.listdir(current_dir+'/test/labels') 
-        
-    print("==============================================")
-    print("Train Image : {}".format(len(trainImageList)))  
-    print("Valid Image : {}".format(len(validImageList)))  
-    print("Test  Image : {}".format(len(testImageList)))
-    print("==============================================")
-
-    if len(trainImageList)!=len(trainLabelList):
-        print("Train Data Copy Error")
-        exit(-1)
-    if len(validImageList)!=len(validLabelList):
-        print("valid Data Copy Error")
-        exit(-1)
-    if len(testImageList)!=len(testLabelList):
-        print("valid Data Copy Error") 
-        exit(-1) 
-    print("Copy is Done")
 
 def moveDataset():
+    global counter
     makeFolder()
-    copyDataset()
-    checkCopy()
+
+    # txt => labels/
+    print('run moveDataset')
+    datasetFileList = []
+    for filelist in glob.iglob(os.path.join(datasetDir, '*.jpg')):
+        datasetFileList.append(filelist)
+
+    # 폴더에서 읽어온 리스트를  random.shuffle함수를 이용해서 마구잡이로 섞어준다.
+    random.shuffle(datasetFileList)
+    print(datasetFileList)
+
+    for pathAndFilename in datasetFileList:
+    #for pathAndFilename in glob.iglob(os.path.join(datasetDir+'/export/images', '*.jpg')):
+        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+
+        labelFilename = os.path.join(datasetDir, title+'.txt')
+        #print(datasetDir)
+        #print(labelFilename)
+        if counter == index_test:
+            counter = 1
+            shutil.copy(pathAndFilename, os.path.join(testImagePath, title+'.jpg'))
+            shutil.copy(labelFilename, os.path.join(testLabelPath, title+'.txt'))
+        elif counter == index_valid:
+            shutil.copy(pathAndFilename, os.path.join(validImagePath, title+'.jpg'))
+            shutil.copy(labelFilename, os.path.join(validLabelPath, title+'.txt'))
+            counter = counter + 1
+        else:
+            shutil.copy(pathAndFilename, os.path.join(trainImagePath, title+'.jpg'))
+            shutil.copy(labelFilename, os.path.join(trainLabelPath, title+'.txt'))
+            counter = counter + 1
+
+
+def checkCopy():
+    trainImageList = os.listdir(trainImagePath)
+    validImageList = os.listdir(validImagePath)
+    testImageList = os.listdir(testImagePath)
+    trainTextList = os.listdir(trainLabelPath)
+    validTextList = os.listdir(validLabelPath)
+    testTextList = os.listdir(testLabelPath)
+    print(len(trainImageList), len(trainTextList))
+    print(len(testImageList), len(testTextList))
+    print(len(validImageList), len(validTextList))
 
 moveDataset()
+checkCopy()
